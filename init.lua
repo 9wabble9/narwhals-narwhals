bd8947b18b541eb7f9b9fd959bcfd17caa3a8948
9wabble9 <9wabble9@proton.me>
2026-06-16 18:48:12 -0300

    asdlkmasdlkmasd idk im going to distrohop because the AUR is lowk ruined lol

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 require("config.remaps")
 require("config.lazy")
+require("config.lsp")
 
 require("lualine").setup()
 require("mason").setup()
@@ -11,3 +12,18 @@ vim.opt.softtabstop = 4
 vim.opt.expandtab = true
 vim.opt.number = true
 vim.opt.relativenumber = true
+
+vim.diagnostic.config({
+    update_in_insert = false,
+	virtual_text = {
+		prefix = "",
+	},
+	signs = {
+		text = {
+			[vim.diagnostic.severity.ERROR] = " ",
+			[vim.diagnostic.severity.WARN] = " ",
+			[vim.diagnostic.severity.INFO] = " ",
+			[vim.diagnostic.severity.HINT] = "󰠠 ",
+		},
+	},
+})
---
+++ lua/config/lsp.lua
@@ -0,0 +1,17 @@
+vim.lsp.config("ts_ls", {})
+vim.lsp.enable("ts_ls")
+
+vim.lsp.config("rust-analyzer", {})
+vim.lsp.enable("rust-analyzer")
+
+vim.lsp.config("pyright", {})
+vim.lsp.enable("pyright")
+
+vim.lsp.config("gopls", {})
+vim.lsp.enable("gopls")
+
+vim.lsp.config("qmlls", {})
+vim.lsp.enable("qmlls")
+
+vim.lsp.config("zls", {})
+vim.lsp.enable("zls")
